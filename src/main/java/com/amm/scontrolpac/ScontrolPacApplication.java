b4ff544cd9654d4d268846fee152c26f31b7990f
Alexandre Mattos <alemattos_32@hotmail.com>
2025-06-26 15:09:42 -0300

    Projeto ScontrolPAC

---
+++ src/main/java/com/amm/scontrolpac/ScontrolPacApplication.java
@@ -0,0 +1,13 @@
+package com.amm.scontrolpac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScontrolPacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScontrolPacApplication.class, args);
+	}
+
+}
